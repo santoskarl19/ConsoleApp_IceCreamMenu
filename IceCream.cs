a450b39ade59cedf62b4c4be7b0e2af073cff997
Karl Santos <103978985+santoskarl19@users.noreply.github.com>
2023-06-24 21:18:58 -0700

    upload files

---
+++ IceCream.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Assignment_IceCream
+{
+    public class IceCream
+    {
+        // get number of orders from user and store in 2D Array
+        public int[,] GetIceCreamOrders(int[,] iceCreamArray)
+        {
+            for (int i = 0; i < iceCreamArray.GetLength(0); i++)
+            {
+                switch (i + 1)
+                {
+
+                    case 1:
+                        Console.WriteLine("[Chocolate Flavor]");
+                        break;
+                    case 2:
+                        Console.WriteLine("[Vanilla Flavor]");
+                        break;
+                    case 3:
+                        Console.WriteLine("[Strawberry Flavor]");
+                        break;
+                    default:
+                        break;
+                }
+
+                for (int j = 0; j < iceCreamArray.GetLength(1); j++)
+                {
+                    switch (j + 1)
+                    {
+                        case 1:
+                            Console.Write("Total small scoops sold: ");
+                            iceCreamArray[i, j] = Int32.Parse(Console.ReadLine());
+                            break;
+                        case 2:
+                            Console.Write("Total medium scoops sold: ");
+                            iceCreamArray[i, j] = Int32.Parse(Console.ReadLine());
+                            break;
+                        case 3:
+                            Console.Write("Total large scoops sold: ");
+                            iceCreamArray[i, j] = Int32.Parse(Console.ReadLine());
+                            break;
+                        default:
+                            break;
+
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            return iceCreamArray;
+        }
+
+        // display data from 2D array
+        public void DisplayOrders(int[,] iceCreamArray)
+        {
+            PrintLineBreak();
+
+            Console.WriteLine("\t\t --ICE CREAM ORDERS--");
+            Console.WriteLine();
+            Console.WriteLine("\t\t SMALL \t MEDIUM  LARGE");
+
+            for (int i = 0; i < iceCreamArray.GetLength(0); i++)
+            {
+                switch (i + 1)
+                {
+
+                    case 1:
+                        Console.Write("[Chocolate]");
+                        break;
+                    case 2:
+                        Console.Write("[Vanilla]");
+                        break;
+                    case 3:
+                        Console.Write("[Strawberry]");
+                        break;
+                    default:
+                        break;
+                }
+
+                for (int j = 0; j < iceCreamArray.GetLength(1); j++)
+                {
+                    Console.Write($"\t {iceCreamArray[i, j]}");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            PrintLineBreak();
+        }
+        
+        // line break
+        static void PrintLineBreak()
+        {
+            Console.WriteLine("----------------------------------------------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_IceCream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IceCream iceCream = new IceCream();
+
+            int[,] iceCreamSold = new int[3, 3];
+
+            iceCreamSold = iceCream.GetIceCreamOrders(iceCreamSold);
+
+            iceCream.DisplayOrders(iceCreamSold);
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
